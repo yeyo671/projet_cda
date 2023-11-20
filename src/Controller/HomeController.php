049d6131fe21f461524711c471149e7c40e8ed30
leotran671 <leo.tran@my-digital-school.org>
2023-11-20 15:00:20 +0100

    Add home page link to the navbar

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function index(PaintRepository $paintRepository, BlogpostRepository $blogpostRepository): Response
     {
         return $this->render('home/index.html.twig', [
