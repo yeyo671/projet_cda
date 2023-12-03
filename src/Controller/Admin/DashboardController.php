3a6cfaa3671bfd72a419916d04b23fd6b0b67bf8
leotran671 <leo.tran@my-digital-school.org>
2023-12-03 21:07:29 +0100

    Installation of EasyAdmin and customization of dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        return $this->render("admin/dashboard.html.twig");
     }
 
     public function configureDashboard(): Dashboard
